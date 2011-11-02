889892728b60840c8d4d6256bd475f1edc86088b
Robert <rtdeloatch@gmail.com>
2011-11-02 02:26:23 -0500

    Added files

---
+++ Classes/Classes-1.moved-aside/ViewCompanies.h
@@ -0,0 +1,16 @@
+//
+//  ViewCompanies.h
+//  ConventionBuddy
+//
+//  Created by Robert Deloatch on 10/8/11.
+//  Copyright 2011 UMBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewCompanies : UITableViewController {
+
+}
+
+@end
